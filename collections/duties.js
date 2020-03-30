36632bf0a385d04d81d13e5ddd4b7aa473b82ba5
omriavidan <omriavidan0402hn@gmail.com>
2020-03-30 16:14:37 +0300

    Implemented duties find

--- collections/duties.js
+++ collections/duties.js
@@ -1,26 +1,32 @@
+
 const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+const ObjectId = require('mongodb').ObjectID
+
 const url = 'mongodb://localhost:27017';
 const dbName = 'myproject';
+const dutiesFields = 7
 
 function checkData(dutiesData) {
-  if (dutiesData["name"] && dutiesData["location"] && dutiesData["days"] && dutiesData["constraints"] && dutiesData["soldiersRequired"] && dutiesData["value"] && Object.keys(dutiesData).length === 7) {
-    return true;
-  }
-  return false;
+  return (dutiesData["name"] != null) &&
+    (dutiesData["location"] != null) &&
+    (dutiesData["days"] != null) &&
+    (dutiesData["constraints"] != null) &&
+    (dutiesData["soldiersRequired"] != null) &&
+    (dutiesData["value"] != null) &&
+    (Object.keys(dutiesData).length === dutiesFields)
 }
 
 function handleInsertion(dutiesData, done) {
-  dutiesData["soldiers"] = [];
+  dutiesData["soldiers"] = dutiesData["soldiers"] || [];
   if (checkData(dutiesData) === false) {
-    done("One or more fields is invalid");
+    done("One or more fields are invalid");
   } else {
     MongoClient.connect(url, function (err, client) {
-      assert.equal(null, err);
+      if(err) return done(err)
       const db = client.db(dbName);
       const collection = db.collection('Duties')
-      collection.insertOne(dutiesData, (err) => {
-        assert.equal(err, null);
+      collection.insertOne(dutiesData, (insertErr) => {
+        if(insertErr) return done(insertErr)
         client.close();
         done(err);
       })
@@ -28,4 +34,31 @@ function handleInsertion(dutiesData, done) {
   }
 }
 
+function handleFind(dutyId, dutyName, done) {
+  MongoClient.connect(url, function (err, client) {
+    if(err) return done(err)
+    const db = client.db(dbName);
+    const collection = db.collection('Duties')
+    let dutyToSearch = {};
+    if (dutyId) {
+      if (/[0-9a-fA-F]{24}/.test(dutyId)) {
+        dutyToSearch["_id"] = ObjectId(dutyId);
+      } else {
+        done("invalid duty ID");
+      }
+    } else if (dutyName) {
+      dutyToSearch["name"] = dutyName;
+    }
+    collection.find(dutyToSearch).toArray(function (findErr, docs) {
+      if(findErr) return done(findErr)
+      if (docs.length === 1) {
+        docs = docs[0];
+      }
+      client.close();
+      done(err, JSON.stringify(docs));
+    });
+  });
+}
+
 module.exports.insertDuty = handleInsertion;
+module.exports.findDuty = handleFind;
