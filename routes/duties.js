8ec0d0c6c0e53542e6db956abc8efa92b2fb6861
omriavidan <omriavidan0402hn@gmail.com>
2020-03-30 16:09:20 +0300

    Implemented duties routs

---
+++ routes/duties.js
@@ -0,0 +1,27 @@
+const duties = require('../collections/duties.js');
+const urlib = require('url');
+
+function handleDutyReq(req, data, done) {
+  const parsedUrl = new urlib.URL("http://localhost:3000/" + req.url);
+  const urlParams = req.url.split("/")[2];
+  const parsedUrlLength = req.url.split("/").length;
+  if (parsedUrlLength >= 5) {
+    return done(new Error())
+  }
+  if (req.method === "GET") {
+    if (parsedUrlLength === 3) {
+      return duties.findDuty(urlParams, null, done)
+    } else if (parsedUrl.searchParams.has("name")) {
+      return duties.findDuty(null, parsedUrl.searchParams.get("name"), done)
+    } else if (parsedUrlLength === 2) {
+      return duties.findDuty(null, null, done)
+    }
+    return done(new Error())
+
+  } else if (req.method === "POST" && parsedUrlLength === 2) {
+    return duties.insertDuty(data, done);
+  }
+  return done(new Error())
+}
+
+module.exports.handleDutyReq = handleDutyReq;
