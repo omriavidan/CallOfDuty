604069fa4d2d27a43e733162aad9bc9ab61467a2
omriavidan <omriavidan0402hn@gmail.com>
2020-04-03 14:47:05 +0300

    Implemented duties update

--- test/routs/testDutiesRouts.js
+++ test/routs/testDutiesRouts.js
@@ -203,7 +203,7 @@ describe("Duties tests", function () {
 
   describe("Duties delete test", function () {
 
-    it("Should be able to return correct respone when trying to delete correct duty", function (done) {
+    it("Should be able to return correct respone when trying to delete correct soldier", function (done) {
       MongoClient.connect(DBurl, function (err, client) {
         assert.equal(null, err);
         const db = client.db(dbName);
@@ -241,9 +241,96 @@ describe("Duties tests", function () {
       });
     })
 
-    it("Should be able to return correct respone when trying to delete non-existent duty", function (done) {
+    it("Should be able to return correct respone when trying to delete correct soldier", function (done) {
       const Http = new XMLHttpRequest();
       Http.open("DELETE", serverUrl + "/435435");
+      Http.send();
+      Http.onreadystatechange = (e) => {
+        if (Http.readyState == 4 && Http.status == 200) {
+          expect(Http.responseText).to.eql("invalid duty ID");
+          done();
+        }
+      }
+    })
+  });
+
+  describe("Duties update test", function () {
+
+    it("Should be able to return correct respone when trying to update correct soldier", function (done) {
+      MongoClient.connect(DBurl, function (err, client) {
+        assert.equal(null, err);
+        const db = client.db(dbName);
+        const collection = db.collection('Duties')
+        collection.insertOne({
+          "name": "hagnash",
+          "location": "soosia",
+          "days": "7",
+          "constraints": "none",
+          "soldiersRequired": "2",
+          "value": "10",
+          "soldiers": []
+        }, (err, docInserted) => {
+          assert.equal(err, null);
+          const Http = new XMLHttpRequest();
+          Http.open("PATCH", serverUrl + "/" + (docInserted["insertedId"].toString()));
+          Http.send(JSON.stringify({
+            "location": "yair",
+            "days": "4"
+          }));
+          Http.onreadystatechange = (e) => {
+            if (Http.readyState == 4 && Http.status == 200) {
+              expect(Http.responseText).to.eql("");
+              collection.deleteOne({
+                "_id": docInserted["insertedId"]
+              }, (deleteError) => {
+                client.close();
+                done();
+              })
+            }
+          }
+        })
+      });
+    })
+
+    it("Should be able to return correct respone when trying to update with wrong fields", function (done) {
+      MongoClient.connect(DBurl, function (err, client) {
+        assert.equal(null, err);
+        const db = client.db(dbName);
+        const collection = db.collection('Duties')
+        collection.insertOne({
+          "name": "hagnash",
+          "location": "soosia",
+          "days": "7",
+          "constraints": "none",
+          "soldiersRequired": "2",
+          "value": "10",
+          "soldiers": []
+        }, (err, docInserted) => {
+          assert.equal(err, null);
+          const Http = new XMLHttpRequest();
+          Http.open("PATCH", serverUrl + "/" + (docInserted["insertedId"].toString()));
+          Http.send(JSON.stringify({
+            "location": "yair",
+            "lol": "4"
+          }));
+          Http.onreadystatechange = (e) => {
+            if (Http.readyState == 4 && Http.status == 200) {
+              expect(Http.responseText).to.eql("One or more fields is invalid");
+              collection.deleteOne({
+                "_id": docInserted["insertedId"]
+              }, (deleteError) => {
+                client.close();
+                done();
+              })
+            }
+          }
+        })
+      });
+    })
+
+    it("Should be able to return correct respone when trying to delete correct soldier", function (done) {
+      const Http = new XMLHttpRequest();
+      Http.open("PATCH", serverUrl + "/435435");
       Http.send();
       Http.onreadystatechange = (e) => {
         if (Http.readyState == 4 && Http.status == 200) {
