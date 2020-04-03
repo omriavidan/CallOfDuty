0d8cf03eaa86b41265d126f0f7f781d0a8ce0f35
omriavidan <omriavidan0402hn@gmail.com>
2020-04-03 14:47:05 +0300

    Implemented duties update

--- collections/duties.js
+++ collections/duties.js
@@ -11,6 +11,16 @@ function checkData(dutiesData) {
   return true;
 }
 
+function checkUpdateData(dutiesData) {
+  let legalAttr = ["name", "location", "days", "constraints", "soldiersRequired", "value", "soldiers"]
+  for (let attr in dutiesData) {
+    if (!(legalAttr.includes(attr))) {
+      return false;
+    }
+  }
+  return true;
+}
+
 function handleInsertion(dutiesData, done) {
   dutiesData["soldiers"] = [];
   if (checkData(dutiesData) === false) {
@@ -71,7 +81,7 @@ function handleDelete(dutyId, done) {
     collection.find(dutyToSearch).toArray(function (err, record) {
       assert.equal(err, null);
       if (record.length === 1 && record[0]["soldiers"].length === 0) {
-        collection.deleteOne(dutyToSearch, (deleteError) => {
+        collection.deleteOne({"_id": ObjectId(dutyId)}, (deleteError) => {
           client.close();
           done(deleteError);
         })
@@ -83,6 +93,43 @@ function handleDelete(dutyId, done) {
   });
 }
 
+function updateDuty(dutyId, dataToUpdate, done) {
+  if (checkUpdateData(dataToUpdate) === false) {
+    done("One or more fields is invalid");
+  } else {
+    MongoClient.connect(url, function (err, client) {
+      assert.equal(null, err);
+      const db = client.db(dbName);
+      const collection = db.collection('Duties')
+      let dutyToSearch = {};
+      if (dutyId) {
+        if (/[0-9a-fA-F]{24}/.test(dutyId)) {
+          dutyToSearch["_id"] = ObjectId(dutyId);
+        } else {
+          done("invalid duty ID");
+        }
+      }
+      collection.find(dutyToSearch).toArray(function (err, record) {
+        assert.equal(err, null);
+        if (record.length === 1 && record[0]["soldiers"].length === 0) {
+          collection.updateOne(dutyToSearch, {
+            $set: dataToUpdate
+          }, function (err, result) {
+            assert.equal(err, null);
+            assert.equal(1, result.result.n);
+            client.close();
+            done(err);
+          });
+        } else {
+          client.close();
+          done("Duty is already scheduled and can't be deleted");
+        }
+      });
+    });
+  }
+}
+
 module.exports.insertDuty = handleInsertion;
 module.exports.findDuty = handleFind;
 module.exports.deleteDuty = handleDelete;
+module.exports.updateDuty = updateDuty;
