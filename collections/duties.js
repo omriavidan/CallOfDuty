c5dd6c9a5b3a6469b1f73c2a53385e73922d8ed5
omriavidan <omriavidan0402hn@gmail.com>
2020-04-03 14:47:05 +0300

    Implemented duties update

--- collections/duties.js
+++ collections/duties.js
@@ -1,30 +1,41 @@
 const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
-<<<<<<< HEAD
 const ObjectId = require('mongodb').ObjectID
-=======
->>>>>>> 3d04f2a... Implemented duties delete
+
 const url = 'mongodb://localhost:27017';
 const dbName = 'myproject';
+const dutiesFields = 7
 
 function checkData(dutiesData) {
-  if (dutiesData["name"] && dutiesData["location"] && dutiesData["days"] && dutiesData["constraints"] && dutiesData["soldiersRequired"] && dutiesData["value"] && Object.keys(dutiesData).length === 7) {
-    return true;
+  return (dutiesData["name"] != null) &&
+    (dutiesData["location"] != null) &&
+    (dutiesData["days"] != null) &&
+    (dutiesData["constraints"] != null) &&
+    (dutiesData["soldiersRequired"] != null) &&
+    (dutiesData["value"] != null) &&
+    (Object.keys(dutiesData).length === dutiesFields)
+}
+
+function checkUpdateData(dutiesData) {
+  let legalAttr = ["name", "location", "days", "constraints", "soldiersRequired", "value", "soldiers"]
+  for (let attr in dutiesData) {
+    if (!(legalAttr.includes(attr))) {
+      return false;
+    }
   }
-  return false;
+  return true;
 }
 
 function handleInsertion(dutiesData, done) {
-  dutiesData["soldiers"] = [];
+  dutiesData["soldiers"] = dutiesData["soldiers"] || [];
   if (checkData(dutiesData) === false) {
-    done("One or more fields is invalid");
+    done("One or more fields are invalid");
   } else {
     MongoClient.connect(url, function (err, client) {
-      assert.equal(null, err);
+      if(err) return done(err)
       const db = client.db(dbName);
       const collection = db.collection('Duties')
-      collection.insertOne(dutiesData, (err) => {
-        assert.equal(err, null);
+      collection.insertOne(dutiesData, (insertErr) => {
+        if(insertErr) return done(insertErr)
         client.close();
         done(err);
       })
@@ -34,7 +45,7 @@ function handleInsertion(dutiesData, done) {
 
 function handleFind(dutyId, dutyName, done) {
   MongoClient.connect(url, function (err, client) {
-    assert.equal(null, err);
+    if(err) return done(err)
     const db = client.db(dbName);
     const collection = db.collection('Duties')
     let dutyToSearch = {};
@@ -47,8 +58,8 @@ function handleFind(dutyId, dutyName, done) {
     } else if (dutyName) {
       dutyToSearch["name"] = dutyName;
     }
-    collection.find(dutyToSearch).toArray(function (err, docs) {
-      assert.equal(err, null);
+    collection.find(dutyToSearch).toArray(function (findErr, docs) {
+      if(findErr) return done(findErr)
       if (docs.length === 1) {
         docs = docs[0];
       }
@@ -60,7 +71,7 @@ function handleFind(dutyId, dutyName, done) {
 
 function handleDelete(dutyId, done) {
   MongoClient.connect(url, function (err, client) {
-    assert.equal(null, err);
+    if(err) return done(err)
     const db = client.db(dbName);
     const collection = db.collection('Duties')
     let dutyToSearch = {};
@@ -71,8 +82,8 @@ function handleDelete(dutyId, done) {
         done("invalid duty ID");
       }
     }
-    collection.find(dutyToSearch).toArray(function (err, record) {
-      assert.equal(err, null);
+    collection.find(dutyToSearch).toArray(function (findErr, record) {
+      if(findErr) return done(findErr)
       if (record.length === 1 && record[0]["soldiers"].length === 0) {
         collection.deleteOne(dutyToSearch, (deleteError) => {
           client.close();
@@ -89,9 +100,41 @@ function handleDelete(dutyId, done) {
   });
 }
 
+function updateDuty(dutyId, dataToUpdate, done) {
+  if (checkUpdateData(dataToUpdate) === false) {
+    done("One or more fields are invalid");
+  } else {
+    MongoClient.connect(url, function (err, client) {
+      if(err) return done(err)
+      const db = client.db(dbName);
+      const collection = db.collection('Duties')
+      let dutyToSearch = {};
+      if (dutyId) {
+        if (/[0-9a-fA-F]{24}/.test(dutyId)) {
+          dutyToSearch["_id"] = ObjectId(dutyId);
+        } else {
+          done("invalid duty ID");
+        }
+      }
+      collection.find(dutyToSearch).toArray(function (findErr, record) {
+        if(findErr) return done(findErr)
+        if (record.length === 1 && record[0]["soldiers"].length === 0) {
+          collection.updateOne(dutyToSearch, {
+            $set: dataToUpdate
+          }, function (err) {
+            client.close();
+            done(err);
+          });
+        } else {
+          client.close();
+          done("Duty is already scheduled and can't be deleted");
+        }
+      });
+    });
+  }
+}
+
 module.exports.insertDuty = handleInsertion;
 module.exports.findDuty = handleFind;
-<<<<<<< HEAD
-=======
 module.exports.deleteDuty = handleDelete;
->>>>>>> 3d04f2a... Implemented duties delete
+module.exports.updateDuty = updateDuty;
